81a4e38c9537e8e2e80240a1a78f6cfed75f02d6
ericabasak <ericabasak@gmail.com>
2019-11-07 11:09:14 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,27 @@
 class HeroinesController < ApplicationController
+  # all records
   def index
     @heroines = Heroine.all
   end
+
+  # singe record
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    redirect_to heroines_path(@heroine)
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power, :power_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  # resources :powers, only: [:index]
+  # resources :heroines, only: [:index]  
+
+  get '/heroines', to: 'heroines#index', as: 'heroines'
+  get '/heroines/new', to: 'heroines#new', as: 'new_heroine'
+  get '/heroines/:id', to: 'heroines#show', as: 'heroine'
+  post '/heroines', to: 'heroines#create'
+
+  get '/powers', to: 'powers#index', as: 'powers'
+  get '/powers/', to: 'powers#show', as: 'power'
+
 end
