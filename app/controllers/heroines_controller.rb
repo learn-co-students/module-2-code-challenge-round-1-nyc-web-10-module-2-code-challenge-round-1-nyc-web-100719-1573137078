a8849b23a7d4da09b7f4e913ae1633289c8d2afd
Bailey Wood <woodbmae@gmail.com>
2019-11-07 10:16:42 -0500

    deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,28 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(strong_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      redirect_to heroines_path
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+    belongs_to :power
+    validates_uniqueness_of :super_name, :case_sensitive => false
 end
