340560d88d9411ba183ff357c7a74d8afd4e7298
Nick <nick.moi@gmail.com>
2019-11-07 10:28:56 -0500

    First commit before doing filter

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  #resources :powers, only: [:index]
+  #resources :heroines, only: [:index]  
+
+  #powers routes
+  get '/powers', to: 'powers#index', as: 'powers'
+  get '/powers/:id', to: 'powers#show', as: 'power'
+
+  #heroines routes
+  get '/heroines', to: 'heroines#index', as: 'heroines'
+  get 'heroines/new', to: 'heroines#new', as: 'new_heroine'
+  post '/heroines', to: 'heroines#create'
+  get 'heroines/:id', to: 'heroines#show', as: 'heroine'
 end
