bc5a42fc2e48eee9f154cf2a7b3cb920c334255b
LainaJ <lainakarosic@gmail.com>
2019-11-07 11:07:08 -0500

    Laina K

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,45 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all 
+  end 
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all 
+  end 
+
+  #if no validations 
+  # def create
+  #   @heroine = Heroine.create(heroine_params)
+  #   redirect_to heroine_path(@heroine)
+  # end 
+
+  #for validations - to revise?
+  #make a new heroine taking in the user's params, 
+  #check for validations, if valid then save and redirect to that new heroine page
+  # if not valid show a new form page to fill out again  
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save 
+      redirect_to heroine_path(@heroine)
+    else
+      redirect_to new_heroine_path
+    end 
+  end 
+
+
+  private 
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end 
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index]
   resources :heroines, only: [:index]  
+
+# heroines routes 
+  #index
+  #get '/heroines', to: 'heroines#index', as: 'heroines'
+  
+  #new
+  get 'heroines/new', to: 'heroines#new', as: 'new_heroine'
+  #create
+  post 'heroines', to: 'heroines#create'
+  #show
+  get '/heroines/:id', to: 'heroines#show', as: 'heroine'
+  
+
+
+
+#powers routes
+#get '/powers', to: 'powers#index', as: 'powers'
+get '/powers/:id', to: 'powers#show', as: 'power'
+#create
+post '/powers', to: 'powers#create'
+#new 
+get 'powers/new', to: 'powers#new', as: 'new_power'
+
 end
