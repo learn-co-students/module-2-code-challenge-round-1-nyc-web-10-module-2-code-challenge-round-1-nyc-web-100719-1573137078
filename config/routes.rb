4f636f34d5d51b14e211408dce8fbaa7c11bbb88
Jay Stewart <ryanstewartmiller@live.com>
2019-11-07 10:44:35 -0500

    Finished dis shiz

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  
+
+
+
+
+  get '/heroines', to: 'heroines#index', as: 'heriones'
+  get '/heroines/new', to: 'heroines#new', as: 'new_heroine'
+  get '/heroines/:id', to: 'heroines#show', as: 'heroine'
+  post '/heroines', to: 'heroines#create'
+
+  get '/powers', to: 'powers#index', as: 'powers'
+  get '/powers/new', to: 'powers#new', as: 'new_power'
+  get '/powers/:id', to: 'powers#show', as: 'power'
+
+
 end
