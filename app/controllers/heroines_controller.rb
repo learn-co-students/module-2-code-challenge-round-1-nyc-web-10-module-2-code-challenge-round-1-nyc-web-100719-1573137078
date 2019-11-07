1bbf2b3117ab3da839775cfe12ebe01f80a107da
hvtsxyz <hvts@msn.com>
2019-11-07 11:07:02 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,35 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = Power.all
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    heroine = Heroine.new(heroine_params)
+    if heroine.save
+      redirect_to heroines_path
+    else
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index,]
+  resources :heroines, only: [:index, :new]  
+
+  get 'powers/:id', to: 'powers#show', as: 'power'
+
+  get 'heroines/:id', to: 'heroines#show', as: 'heroine'
+  post '/heroines', to: 'heroines#create'
 end
