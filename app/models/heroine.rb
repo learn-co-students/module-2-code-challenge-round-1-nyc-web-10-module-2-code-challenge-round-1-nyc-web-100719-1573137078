a55d5b9bb66b4616f93d6f4581af12b6aeb12569
Jay Stewart <ryanstewartmiller@live.com>
2019-11-07 10:47:27 -0500

    Changed validations slightly

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -2,5 +2,6 @@ class Heroine < ApplicationRecord
 
     belongs_to :power
 
-    validates :name, uniqueness: true 
+    validates :super_name, uniqueness: true 
+    validates :name, presence: true 
 end
