e4d59c030da6377ac911f3e0f3e5ba3a1f2e01e8
github username <github email address>
2019-11-07 10:53:00 -0500

    Bonus Deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,10 +1,21 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    @heroine = Heroine.new
+    @powers = Power.all
   end
   
   def show
     @heroine = Heroine.find(params[:id])
+  end
+
+  def filter
+    @powers = Power.all
+    @power_id = filter_params[:power_id].to_i
+    @heroines_filtered = Heroine.all.select do |heroine|
+      heroine.power_id == @power_id
+    end
+    render :index
   end
 
   def new
@@ -22,12 +33,19 @@ class HeroinesController < ApplicationController
       render :new
     end
     
+    
+
+
   end
 
   private
 
   def heroine_params
     params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def filter_params
+    params.require(:heroine).permit(:power_id)
   end
 
 end
